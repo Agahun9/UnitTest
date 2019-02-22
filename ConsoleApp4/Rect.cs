ce0f55fb2bdabd186e332ff24614b6fba9760475
Konrad Piech <k9602@o2.pl>
2019-02-22 20:05:59 +0100

    drawing rectangle class

---
+++ ConsoleApp4/Rect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Rect
+{
+    public class Rect
+    {
+        int ln;
+        int w;
+        IRectMethod rectMethod;
+
+        public Rect(int ln, int w, IRectMethod rectMethod)
+        {
+
+            if (ln <= 0 || w <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (rectMethod == null)
+            {
+                throw new ArgumentNullException();
+            }
+            this.rectMethod = rectMethod;
+            this.ln = ln;
+            this.w = w;
+        }
+
+        public void Draw()
+        {
+            this.rectMethod.rectDraw(this.ln, this.w);
+        }
+    }
+    public interface IRectMethod
+    {
+        void rectDraw(int ln, int w);
+    }
+}
---
+++ UnitTestProject2/RectTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Rect
+{
+    [TestClass]
+    public class RectTests
+    {
+
+        [TestMethod]
+        public void CreateCorrectReact_Passing()
+        {
+            IRectMethod rectMethod = new Mock<IRectMethod>().Object;
+            new Rect(1, 1, rectMethod);
+        }
+
+      
+    }
+}
